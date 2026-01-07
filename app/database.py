83601950628518df82da0690fb5f490886d59e71
unknown <shayan.aimoradii@gmail.com>
2026-01-08 03:14:03 +0330

    fix: SQLALCHEMY_DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@pg_db:5432/postgres"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@postgres:5432/postgres" 
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,  # connect_args={"check_same_thread": False}
